b63ada101461294e4ca6bd124a0ebea1ad075b09
John Berlanga <jberlanga748@gmail.com>
2026-05-22 15:54:47 -0700

    Extract Time command rail components

--- src/time-route-components-import.test.js
+++ src/time-route-components-import.test.js
@@ -11,9 +11,11 @@ test("App imports extracted time summary cards", () => {
   assert.doesNotMatch(appSource, /function workCategoryLabel\(/);
 
   assert.match(routeComponentsSource, /export function TimeEntriesTable\b/);
+  assert.match(routeComponentsSource, /export function TimeJobCostingReadinessCard\b/);
   assert.doesNotMatch(appSource, /function TimeEntriesTable\(/);
+  assert.doesNotMatch(appSource, /function TimeJobCostingReadinessCard\(/);
 
-  for (const name of ["ActiveTimeCard", "RecentTimeEntriesCard", "TimeEntriesTablePolished", "TimeEntryCard", "TimeKpiCardPolished", "TimeMobileAccordionCard", "TimeMobileFieldGroup", "TimeStatusBadge", "TimeSummaryMetricsPolished", "WeekSummaryCard"]) {
+  for (const name of ["ActiveTimeCard", "RecentTimeEntriesCard", "TimeCommandRailPolished", "TimeCorrectionPanel", "TimeEntriesTablePolished", "TimeEntryCard", "TimeKpiCardPolished", "TimeMobileAccordionCard", "TimeMobileFieldGroup", "TimeStatusBadge", "TimeSummaryMetricsPolished", "WeekSummaryCard"]) {
     assert.match(routeComponentsSource, new RegExp(`export function ${name}\\b`));
     assert.match(appSource, new RegExp(`import \\{[^}]*${name}[^}]*\\} from "\\./time-route-components"`, "s"));
     assert.doesNotMatch(appSource, new RegExp(`function ${name}\\(`));
