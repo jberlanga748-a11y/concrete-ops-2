cdca0b52d11cc411762ebe4ee2ae5f1eef7d31b3
John Berlanga <jberlanga748@gmail.com>
2026-05-22 15:30:44 -0700

    Extract Time entries table views

--- src/time-route-components-import.test.js
+++ src/time-route-components-import.test.js
@@ -10,7 +10,10 @@ test("App imports extracted time summary cards", () => {
   assert.match(appSource, /import \{[^}]*workCategoryLabel[^}]*\} from "\.\/time-route-components"/s);
   assert.doesNotMatch(appSource, /function workCategoryLabel\(/);
 
-  for (const name of ["ActiveTimeCard", "RecentTimeEntriesCard", "TimeEntryCard", "TimeKpiCardPolished", "TimeMobileAccordionCard", "TimeMobileFieldGroup", "TimeStatusBadge", "TimeSummaryMetricsPolished", "WeekSummaryCard"]) {
+  assert.match(routeComponentsSource, /export function TimeEntriesTable\b/);
+  assert.doesNotMatch(appSource, /function TimeEntriesTable\(/);
+
+  for (const name of ["ActiveTimeCard", "RecentTimeEntriesCard", "TimeEntriesTablePolished", "TimeEntryCard", "TimeKpiCardPolished", "TimeMobileAccordionCard", "TimeMobileFieldGroup", "TimeStatusBadge", "TimeSummaryMetricsPolished", "WeekSummaryCard"]) {
     assert.match(routeComponentsSource, new RegExp(`export function ${name}\\b`));
     assert.match(appSource, new RegExp(`import \\{[^}]*${name}[^}]*\\} from "\\./time-route-components"`, "s"));
     assert.doesNotMatch(appSource, new RegExp(`function ${name}\\(`));
